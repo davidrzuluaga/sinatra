08e054c89334466699aed4f043a5c78071c4178c
DavidRZuluaga <git@davidrzuluaga.com>
2018-08-03 00:49:58 -0500

    aaa

--- solution.rb
+++ solution.rb
@@ -1,29 +1,29 @@
 require 'sinatra'
 
+get '/' do
+  erb :index
+end
+
+post '/hello' do
+  "¡Hola #{params[:name]}!"
+  
+end
+
 # get '/' do
+#   @number = request.cookies['number'].to_i
+#   @number||=0
 #   erb :index
 # end
 
-# post '/hello' do
-#   "¡Hola #{params[:name]}!"
-  
+
+# get '/count' do
+#   @number ||= 0
+#   @number = request.cookies['number'].to_i
+#   @number += 1
+#   response.set_cookie('number', value: @number)
+
+#   @number
+#   redirect '/'
+#   erb :index
+
 # end
-
-get '/' do
-  @number = request.cookies['number'].to_i
-  @number||=0
-  erb :index
-end
-
-
-get '/count' do
-  @number ||= 0
-  @number = request.cookies['number'].to_i
-  @number += 1
-  response.set_cookie('number', value: @number)
-
-  @number
-  redirect '/'
-  erb :index
-
-end
