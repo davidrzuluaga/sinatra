c22f0282514d5e75259138bd8c2529809e0beb41
DavidRZuluaga <git@davidrzuluaga.com>
2018-08-02 22:50:47 -0500

    aaa

--- solution.rb
+++ solution.rb
@@ -1,22 +1,5 @@
 require 'sinatra'
 
-get '/makers/:nombre' do
-  if params[:nombre] == nil
-    "Hola desconocido!"
-  elsif params[:nombre] == ""
-    "Hola desconocido!"
-  else
-    "Hola #{params[:nombre].capitalize}!"
-  end
-end
-
-get '/makers' do
-  if params[:nombre] == nil
-    "Hola desconocido!"
-  elsif params[:nombre] == ""
-    "Hola desconocido!"
-  else
-    "Hola #{params[:nombre].capitalize}!"
-  end
-
+post '/hello' do
+  "¡Hola #{params[:name]}!"
 end
