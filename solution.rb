85a12f7867fc13dfc28346369560d860baa7585b
DavidRZuluaga <git@davidrzuluaga.com>
2018-08-04 01:08:32 -0500

    ejercicio8tochk

--- solution.rb
+++ solution.rb
@@ -1,14 +1,23 @@
 require 'sinatra'
 
-get '/' do
+# get '/' do
+#   @function
+#   erb :index
+# end
+
+# get '/stats' do
+#   @function = params[:stats]
+#   @function
   
-  erb :index
+#   erb :index
+# end
+
+get '/' do
+  header = request.env["HTTP_USER_AGENT"].to_s
+  "#{header}"
+  # erb :index
 end
 
-get '/stats' do
-  @stats = params[:stats]
-  @stats
-  redirect '/'
-  erb :index
- 
-end
+# <form action="/stats">
+#  <input type="hidden" value="myFunction()" name="stats">
+# </form>
